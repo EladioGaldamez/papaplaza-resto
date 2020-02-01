491c796282058af42b322b16d1c46fb655ff4b5a
José Galdámez <eladio.galdamez@gmail.com>
2020-02-01 14:56:16 -0600

    Updates

---
+++ precache-manifest.87d2dd806516f4df0b70ab635249cb65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e2ca5f5ce93d2103a8c78b03b7f160",
+    "url": "/papaplaza-resto/index.html"
+  },
+  {
+    "revision": "a6429c3edf4cd5774d72",
+    "url": "/papaplaza-resto/static/css/main.b2a082f6.chunk.css"
+  },
+  {
+    "revision": "f1d6d1d6b253badde5c9",
+    "url": "/papaplaza-resto/static/js/2.b071b50f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/papaplaza-resto/static/js/2.b071b50f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6429c3edf4cd5774d72",
+    "url": "/papaplaza-resto/static/js/main.829707c4.chunk.js"
+  },
+  {
+    "revision": "0a4bf3e136b5bff43b12",
+    "url": "/papaplaza-resto/static/js/runtime-main.dba929d8.js"
+  }
+]);
